317bfcbcf8d99a3bf9d24b60f0cbca5a45dad25c
Marin Vidovic <marin.s.vidovic@hotmail.com>
2020-11-30 12:39:24 +0100

    Routes update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Kontakt aplikacija." });
 });
 
-require("./app/routes/contact.routes")(app);
+require("./app/routes/index")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
